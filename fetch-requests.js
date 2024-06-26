8df49f604d00ad7749457e0a28480e3faf684533
Tyythedeveloper33 <webstertyler81@yahoo.com>
2024-06-26 19:47:10 -0400

    making progress

--- fetch-requests.js
+++ fetch-requests.js
@@ -2,19 +2,33 @@
 Make fetch requests in the browser for each of the following phases.
 Paste your code for fetch requests here once you finish each phase.
 */
-
+// `application/x-www-form-urlencoded`
+// POST
 /* ============================== Phase 1 ============================== */
+function fetchNewProduct() {
+    let url = "http://localhost:5000/products"
+     fetch(url, {
+        method: 'POST',
+        body: { name: 'shed', description: 'tools', price: '20.99', categories: 'grocery' },
 
-// Your code here 
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+    })
+    ;
+}
+
+fetchNewProduct()
+// Your code here
 
 
 
 /* ============================== Phase 2 ============================== */
 
-// Your code here 
+// Your code here
 
 
 
 /* ============================== Phase 3 ============================== */
 
-// Your code here 
+// Your code here
